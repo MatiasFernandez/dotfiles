f91ba1c9baa54b6e4cbad7d1b760386e0ef0530d
Matias Fernandez <mfernandez@10pines.com>
2016-08-31 09:17:15 -0300

    Cleanup bootstrap. Include automatic sourcing of .bashrc.mine

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -130,13 +130,18 @@ install_dotfiles () {
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  echo $DOTFILES_ROOT
-
   for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
   do
     dst="$HOME/.$(basename "${src%.*}")"
     link_file "$src" "$dst"
   done
+
+  if [ `fgrep -c "bashrc.mine" ~/.bashrc` -eq 0 ]; then
+    echo "source ~/.bashrc.mine" >> ~/.bashrc
+    success "Added sourcing of .bashrc.mine to .bashrc"
+  else
+    success "Skipped sourcing of .bashrc.mine"
+  fi
 }
 
 #setup_gitconfig
